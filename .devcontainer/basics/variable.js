46113ef8361c5b8accb7175e4f614ebcd832dd84
Ayush4404 <143592428+Ayush4404@users.noreply.github.com>
2024-01-26 23:59:27 +0000

    basisc

---
+++ .devcontainer/basics/variable.js
@@ -0,0 +1,14 @@
+const accountId = 1443
+let accountEmail ="ayush@234"
+var accountPassword ="1234"
+accountCity = "jaipur"
+let accountState;
+/*const for constant but for variable we can use let or var but we dont use var because it doesnot
+understand the scope .*/
+
+//accountId = 2 // not allowed
+accountEmail ="hitesh@123"
+accountPassword = "1212"
+accountCity ="goa"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])

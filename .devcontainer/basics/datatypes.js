e5811de09e6ba72721f7305d94c2cafa23a82ea1
Ayush4404 <143592428+Ayush4404@users.noreply.github.com>
2024-01-27 00:23:07 +0000

    datatypes

---
+++ .devcontainer/basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all code with new version
+
+//alert(3+3) we are using node js not browser
+
+console.log(3+3)//code readability should be high
+
+console.log("hitesh")
+
+let name ="ayush" //string
+let age = 18 //number
+let isLogged = false //boolean
+let state;
+
+//bigint for big no
+//string =>""
+//boolean=> true/false
+//null=>stand alone value 
+//undefined=> only declared variable but not defined
+//symbol  =>unique
+
+
+//object
+
+console.log(typeof "ayush");
+console.log(typeof null);// undefined is a type but null is part of object
+console.log(typeof undefined);
